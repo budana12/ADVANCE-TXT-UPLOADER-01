2d77344a82fec93506b980cd8d0abbbbe57a5b46
budana12 <sirowabudana@gmail.com>
2025-05-16 14:01:35 +0530

    main.py

--- main.py
+++ main.py
@@ -618,8 +618,8 @@ async def upload(bot: Client, m: Message):
                 name = f'{str(count).zfill(3)}) {name1[:60]}'
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
 
-            elif "https://appx-transcoded-videos-mcdn.akamai.net.in/videos/boosteracademy-data/" in url:
-                url = url.replace("https://appx-transcoded-videos-mcdn.akamai.net.in/videos/boosteracademy-data/", "")
+            elif "https://transcoded-videos-v2.classx.co.in/videos/boosteracademy-data/" in url:
+                url = url.replace("https://transcoded-videos-v2.classx.co.in/videos/boosteracademy-data/", "")
                 name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "@").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
                 name = f'{str(count).zfill(3)}) {name1[:60]}'
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
